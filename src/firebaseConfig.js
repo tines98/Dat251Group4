52947c55dcc45349d52fc6e924d7ae36df7374a6
Tines Valen <tinesvalen@gmail.com>
2022-03-15 14:18:02 +0100

    possibly dangerous firebaseconfig push

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyAipxfz7Efqo7Nos_vNJ369x8sF9Ephc8c",
+    authDomain: "rentivity-d61fc.firebaseapp.com",
+    databseURL: "https://rentivity-d61fc-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "rentivity-d61fc",
+    storageBucket: "rentivity-d61fc.appspot.com",
+    messagingSenderId: "151221540558",
+    appId: "1:151221540558:web:3fc40e378a5ea69336a29f"
 };
 
 export {
