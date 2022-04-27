1de8f7afea309e92f3a92dff638fff82c22da22c
Tines Valen <tinesvalen@gmail.com>
2022-04-27 13:24:12 +0200

    implemented list view and simple filtering

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -14,6 +14,30 @@ function getItemList(setItemsList){
     });
 }
 
+function doFilter(item, keyword){
+    if (keyword===""){
+        return true
+    }
+    for (var key in item){
+        if (item[key] === keyword){
+            return true
+        }
+    }
+    console.log("Nope")
+    return false
+}
+
+const FilterField = (props) => {
+    const [keyword, setKeyword] = useState("");
+    return (
+        <div>
+            <input form="text" onChange={(e) => {setKeyword(e.target.value)}}>
+            </input>
+            <input type="button" onClick={() => props.setFilterKeyword(keyword)} value="Search"></input>
+        </div>
+    );
+}
+
 const ItemListing = (props) => {
     return (
         <div className='ItemListing'>
@@ -31,28 +55,30 @@ const ItemListing = (props) => {
 
 const ListItems = (props) => {
     return (
-        <div className='ItemList'>
-          {props.items.map(item => (
-            <ItemListing item={item}></ItemListing>
-          ))}
+        <div className='ItemList'> {
+            props.items.filter((item) => doFilter(item, props.filterKeyword)
+            ).map((item) => (
+                <ItemListing item={item}></ItemListing>
+            ))
+        }
         </div>
     );
 }
 
 const Welcome = () => {
     const [itemsList, setItemsList] = useState([]);
+    const [filterKeyword, setFilterKeyword] = useState("");
     return (
         <div className="Welcome">
             <img src="https://res.cloudinary.com/simpleview/image/upload/v1504857098/clients/norway/hiking_litlefjellet_andalsnes_norway_2_1_9133c8d8-8524-4326-9c33-de8358fa370a.jpg" width="750"></img>
             <h1>Welcome!</h1>
             <h2>Search for an item</h2>
-            <button onClick={() => {
-                getItemList(setItemsList);
-                }}>
+            <button onClick={() => {getItemList(setItemsList);}}>
                 Load Items
             </button>
+            <FilterField setFilterKeyword={setFilterKeyword}></FilterField>
             <div>
-                <ListItems items={itemsList}/>
+                <ListItems items={itemsList} filterKeyword={filterKeyword}/>
             </div>
 
         </div>
